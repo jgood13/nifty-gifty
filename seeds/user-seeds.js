63eacd503721666157929988372ced1c4e7142e8
Joshua Booth <joshuabenzelli@gmail.com>
2022-08-03 21:41:51 -0600

    Fixed user-seeds.js

---
+++ seeds/user-seeds.js
@@ -0,0 +1,32 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    id: 1,
+    name: "Test",
+    email: "test@test.com",
+    password: "password",
+  },
+  {
+    id: 2,
+    name: "Mandrake",
+    email: "mandrake@test.com",
+    password: "password",
+  },
+  {
+    id: 3,
+    name: "Justin",
+    email: "justin@test.com",
+    password: "password",
+  },
+  {
+    id: 4,
+    name: "Brandon",
+    email: "brandon@test.com",
+    password: "password",
+  },
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
