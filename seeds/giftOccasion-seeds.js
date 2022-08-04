856a4575486b713b1124b6bcb894a39174266559
jgood13 <joseph.good8@gmail.com>
2022-08-03 21:23:34 -0600

    finished a couple seeds, edit package.json scripts

--- seeds/giftOccasion-seeds.js
+++ seeds/giftOccasion-seeds.js
@@ -1,10 +1,35 @@
 const { GiftOccasion } = require("../models");
 
-const registryGiftData = [
+const giftOccasion = [
   {
-    occasion: "Shoes",
+    occasion: "Birthday",
+    occasionDate: 2022 - 11 - 18,
+    giftee_id: 1,
+  },
+  {
+    occasion: "Christmas",
+    occasionDate: 2022 - 12 - 25,
+    giftee_id: 2,
+  },
+  {
+    occasion: "Wedding",
     occasionDate: 2022 - 09 - 18,
     giftee_id: 3,
+  },
+  {
+    occasion: "Anniversary",
+    occasionDate: 2022 - 10 - 24,
+    giftee_id: 4,
+  },
+  {
+    occasion: "Christmas",
+    occasionDate: 2022 - 12 - 25,
+    giftee_id: 5,
+  },
+  {
+    occasion: "Anniversary",
+    occasionDate: 2022 - 12 - 18,
+    giftee_id: 6,
   },
 ];
 
