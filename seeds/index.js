e8099a4f0063055e095b28840f9194d66dcedaeb
jgood13 <joseph.good8@gmail.com>
2022-08-06 11:09:48 -0600

    fixed seed order

--- seeds/index.js
+++ seeds/index.js
@@ -2,19 +2,19 @@ const sequelize = require("../config/connection");
 
 const seedUsers = require("./user-seeds");
 const seedGiftees = require("./giftee-seeds");
+const seedGiftOccasions = require("./giftOccasion-seeds");
 const seedRegistry = require("./registry-seeds");
 const seedGiftIdeas = require("./giftIdea-seeds");
-const seedGiftOccasions = require("./giftOccasion-seeds");
 const seedRegistryGifts = require("./registryGift-seeds");
 
 const seedDB = async () => {
   await sequelize.sync({ force: true });
   await seedUsers();
   await seedGiftees();
+  await seedGiftOccasions();
   await seedGiftIdeas();
   await seedRegistry();
   await seedRegistryGifts();
-  await seedGiftOccasions();
   process.exit(0);
 };
 
